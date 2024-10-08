812a2bf0e23115b2fa0bdea41976c4bbbb12ce9c
David Kagan <david.kagan07@gmail.com>
2024-10-08 13:28:22 -0400

    feat: add concept of teams
    Now there's a section under the search results for your current team.
    There is now a icon button (trash can) that will remove that pokemon
    from your team. The button to add a poke to your team will also
    dissappear if your team is >= 6. Still need to css my way to better
    alignment and prettier things

--- frontend/pages/components/pokemon-stats.tsx
+++ frontend/pages/components/pokemon-stats.tsx
@@ -2,94 +2,111 @@ import localFont from "next/font/local";
 import { Pokemon } from "@/lib/pokemon";
 import { BarColor, MakeWordReadable } from "@/lib/utils";
 import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
 
+// MAX_VALUE is the max value of a single base stat
 const MAX_VALUE = 255;
 
 const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "../fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
-export default function PokemonStats({ pokemon }: { pokemon: Pokemon }) {
-  if (pokemon === undefined || pokemon.name === "") {
-    return <></>;
-  }
+export default function PokemonStats({
+    pokemon,
+    pokemonTeam,
+    addTeamAction,
+}: {
+    pokemon: Pokemon;
+    pokemonTeam: Pokemon[];
+    addTeamAction: Dispatch<SetStateAction<Pokemon[]>>;
+}) {
+    if (pokemon === undefined || pokemon.name === "") {
+        return <></>;
+    }
 
-  // Get the stat total of a pokemon and fills out object of stats
-  const statsObj: { [key: string]: number } = {};
-  let total = 0;
-  pokemon.stats.map((stat) => {
-    statsObj[stat.stat.name] = stat.base_stat;
-    total += stat.base_stat;
-  });
+    // Get the stat total of a pokemon and fills out object of stats
+    const statsObj: { [key: string]: number } = {};
+    let total = 0;
+    pokemon.stats.map((stat) => {
+        statsObj[stat.stat.name] = stat.base_stat;
+        total += stat.base_stat;
+    });
 
-  return (
-    <div
-      id="pokemon-stats-card"
-      className="flex flex-col m-3 items-center h-auto w-[365px] border border-black rounded-lg"
-    >
-      <div className="flex justify-evenly pt-4">
-        <Image
-          src={pokemon.sprites.front_default}
-          alt="pokemon-sprite"
-          width={96}
-          height={96}
-          className="pr-2 space-x-[30px]"
-        />
-        <div className="flex-col">
-          {pokemon.types.map((type, i) => (
-            <div key={i} className="flex pt-3">
-              <Image
-                src={`/${type.type.name}.svg`}
-                alt="type icon"
-                height={25}
-                width={25}
-                className="pr-1"
-              />
-              <p>{MakeWordReadable(type.type.name)}</p>
+    return (
+        <div
+            id="pokemon-stats-card"
+            className="flex flex-col m-3 items-center h-auto w-[365px] border border-black rounded-lg"
+        >
+            <div className="flex justify-evenly pt-4">
+                <Image
+                    src={pokemon.sprites.front_default}
+                    alt="pokemon-sprite"
+                    width={100}
+                    height={100}
+                    className="pr-2 space-x-[30px]"
+                />
+                <div className="flex-col">
+                    {pokemon.types.map((type, i) => (
+                        <div key={i} className="flex pt-3">
+                            <Image
+                                src={`/${type.type.name}.svg`}
+                                alt="type icon"
+                                height={25}
+                                width={25}
+                                className="pr-1"
+                            />
+                            <p>{MakeWordReadable(type.type.name)}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
+
+            <h2 className={`${geistSans.className} bold`}>
+                {pokemon.name.toUpperCase()}
+            </h2>
+            <div className="pt-2">
+                {Object.keys(statsObj).map((key, i) => (
+                    <div key={i} className="inline-flex w-full py-1">
+                        <div className="flex justify-end w-[155px] pr-2 align-middle">
+                            <p className="align-middle pr-1">
+                                {MakeWordReadable(key)}:{"  "}
+                            </p>
+                            <b className="align-middle">{statsObj[key]}</b>
+                        </div>
+                        <div
+                            id="progress-bar-container"
+                            className="flex items-center justify-start w-[200px] h-[30px] align-middle"
+                        >
+                            <div
+                                id="progress-bar"
+                                className="h-[20px] border border-black rounded-md indent-1 text-sm"
+                                style={{
+                                    width: `${(statsObj[key] / MAX_VALUE) * 100}%`,
+                                    background: BarColor(statsObj[key]),
+                                }}
+                            ></div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div id="stat-footer">
+                <b>TOTAL: {total}</b>
+                {pokemonTeam.length >= 6 ? (
+                    <></>
+                ) : (
+                    <button
+                        className="py-1 px-1 my-1 mx-3 bg-gray-200 hover:bg-gray-300 rounded-lg h-100% w-auto font-semibold"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            addTeamAction([...pokemonTeam, pokemon]);
+                        }}
+                    >
+                        Add to team
+                    </button>
+                )}
+            </div>
         </div>
-      </div>
-
-      <h2 className={`${geistSans.className} bold`}>
-        {pokemon.name.toUpperCase()}
-      </h2>
-      <div className="pt-2">
-        {Object.keys(statsObj).map((key, i) => (
-          <div key={i} className="inline-flex w-full py-1">
-            <div className="flex justify-end w-[155px] pr-2 align-middle">
-              <p className="align-middle pr-1">
-                {MakeWordReadable(key)}:{"  "}
-              </p>
-              <b className="align-middle">{statsObj[key]}</b>
-            </div>
-            <div
-              id="progress-bar-container"
-              className="flex items-center justify-start w-[200px] h-[30px] align-middle"
-            >
-              <div
-                id="progress-bar"
-                className="h-[20px] border border-black rounded-md indent-1 text-sm"
-                style={{
-                  width: `${(statsObj[key] / MAX_VALUE) * 100}%`,
-                  background: BarColor(statsObj[key]),
-                }}
-              ></div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div id="stat-footer">
-        <b>TOTAL: {total}</b>
-        {/* some button will go here to add to team */}
-      </div>
-    </div>
-  );
+    );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,48 +1,80 @@
 import { useState } from "react";
 import PokemonStats from "./components/pokemon-stats";
 import { InitialPokeSetter, Pokemon } from "@/lib/pokemon";
+import MiniCard from "./components/pokemon-mini-card";
 // import Conditional from "./components/conditional";
 
 export default function Home() {
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemon, setPokemon] = useState<Pokemon>(InitialPokeSetter);
+    const [pokemonName, setPokemonName] = useState("");
+    const [pokemon, setPokemon] = useState<Pokemon>(InitialPokeSetter);
+    const [pokemonTeam, setPokemonTeam] = useState<Pokemon[]>([]);
 
-  const fetchMessage = async (name: string) => {
-    console.log({ name });
-    // const response = await fetch(`http://localhost:3001/${name}`);
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/jigglypuff/`,
+    console.log({ pokemonTeam });
+
+    const fetchMessage = async (name: string) => {
+        // For names that have a space, ex. Iron hands, we need to change it to:
+        // "iron-hands"
+        name = name.trim().replace(" ", "-").toLowerCase();
+        console.log({ name });
+        const response = await fetch(`http://localhost:3001/${name}`);
+
+        // This is just for local, FE-only development:
+        // const response = await fetch(`https://pokeapi.co/api/v2/pokemon/clefable/`);
+
+        const data = await response.json();
+        console.log({ data });
+
+        setPokemon(data);
+    };
+
+    return (
+        <div className="flex flex-col p-2">
+            <p className="bold text-2xl mx-3 p-2">Search Pokemon Stats</p>
+            <form id="pokename-form" className="flex">
+                <input
+                    id="pokename"
+                    type="text"
+                    name="pokename"
+                    onChange={(e) => setPokemonName(e.target.value)}
+                    placeholder="Search pokemon"
+                    className="placeholder:italic placeholder:text-gray-400 placeholder:p-1 border border-gray-300 rounded-md h-100% mx-3 "
+                />
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        fetchMessage(pokemonName);
+                    }}
+                    className="py-1 px-3 bg-gray-200 hover:bg-gray-300 rounded-lg h-100% font-semibold"
+                >
+                    Search
+                </button>
+            </form>
+
+            <PokemonStats
+                pokemon={pokemon}
+                pokemonTeam={pokemonTeam}
+                addTeamAction={setPokemonTeam}
+            />
+
+            {/* Conditionally showing the words "Your Team:". We only want this
+            if the person already has a team*/}
+            {pokemonTeam.length === 0 ? (
+                <></>
+            ) : (
+                <div className="flex justify-center align-middle pt-3">
+                    <p className="text-2xl font-bold ">Your team:</p>
+                </div>
+            )}
+            <div className="grid grid-cols-3 gap-2 w-full place-items-center p-2">
+                {pokemonTeam.map((mon, i) => (
+                    <MiniCard
+                        key={i}
+                        pokemon={mon}
+                        team={pokemonTeam}
+                        setTeam={setPokemonTeam}
+                    />
+                ))}
+            </div>
+        </div>
     );
-    const data = await response.json();
-    console.log({ data });
-
-    setPokemon(data);
-  };
-
-  return (
-    <div className="p-2">
-      <p className="bold text-2xl mx-3 p-2">Search Pokemon Stats</p>
-      <form id="pokename-form" className="flex">
-        <input
-          id="pokename"
-          type="text"
-          name="pokename"
-          onChange={(e) => setPokemonName(e.target.value)}
-          placeholder="Search pokemon"
-          className="placeholder:italic placeholder:text-gray-400 placeholder:p-1 border border-gray-300 rounded-md h-100% mx-3 "
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            fetchMessage(pokemonName);
-          }}
-          className="py-1 px-3 bg-gray-200 hover:bg-gray-300 rounded-lg h-100% font-semibold"
-        >
-          Search
-        </button>
-      </form>
-
-      <PokemonStats pokemon={pokemon} />
-    </div>
-  );
 }
